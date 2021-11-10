d7580a092e64b087dc68af5a8257645a217aaa01
Kaktotaks <leonidshevchenko350@gmail.com>
2021-11-10 13:36:57 +0200

    Creation account added

--- Milky/Milky/Controllers/LVCConstraints.swift
+++ Milky/Milky/Controllers/LVCConstraints.swift
@@ -14,29 +14,34 @@ extension LoginViewController {
     func setupLoginViewUI() {
     
     view.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
-        
-        let milkyLabel = UILabel()
+    
         milkyLabel.text = " Milky 🐮"
         milkyLabel.textAlignment = .center
-        milkyLabel.textColor = UIColor(red: 96/255, green: 148/255, blue: 176/255, alpha: 0.70)
+        milkyLabel.font = UIFont.systemFont(ofSize: 50, weight: .black)
+        milkyLabel.textColor = UIColor(red: 96/255,
+                                       green: 148/255,
+                                       blue: 176/255,
+                                       alpha: 0.70)
         milkyLabel.numberOfLines = 0
-        milkyLabel.font = UIFont.systemFont(ofSize: 50)
-        
         view.addSubview(milkyLabel)
         milkyLabel.snp.makeConstraints { make in
             make.left.right.equalToSuperview().inset(30)
             make.width.equalTo(300)
-            make.top.equalToSuperview().inset(150)
+            make.top.equalToSuperview().inset(80)
             make.height.equalTo(100)
             
         }
 
-        
-        let emailField = UITextField()
         emailField.placeholder = "    Email Address"
         emailField.font = UIFont.systemFont(ofSize: 20)
-        emailField.backgroundColor = UIColor(red: 96/255, green: 148/255, blue: 176/255, alpha: 50/255)
+        emailField.backgroundColor = UIColor(red: 96/255,
+                                             green: 148/255,
+                                             blue: 176/255,
+                                             alpha: 50/255)
         emailField.layer.cornerRadius = 10
+        emailField.autocapitalizationType = .none
+        emailField.leftViewMode = .always
+        emailField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
         view.addSubview(emailField)
         emailField.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
@@ -44,12 +49,14 @@ extension LoginViewController {
             make.top.equalTo(milkyLabel).inset(100)
             make.height.equalTo(40)
         }
-        
-        let passwordField = UITextField()
+    
         passwordField.placeholder = "    Password"
         passwordField.font = emailField.font
         passwordField.backgroundColor = emailField.backgroundColor
         passwordField.layer.cornerRadius = 10
+        passwordField.autocapitalizationType = .none
+        passwordField.leftViewMode = .always
+        passwordField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
         view.addSubview(passwordField)
         passwordField.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
@@ -59,9 +66,11 @@ extension LoginViewController {
             
         }
         
-        let loginButton = UIButton()
-        loginButton.backgroundColor = UIColor(red: 73/255, green: 166/255, blue: 125/255, alpha: 1)
-//        loginButton.addTarget(self, action: #selector(loginButtonPressed), for: .touchUpInside)
+        loginButton.backgroundColor = UIColor(red: 73/255,
+                                              green: 166/255,
+                                              blue: 125/255,
+                                              alpha: 1)
+        loginButton.addTarget(self, action: #selector(loginButtonPressed), for: .touchUpInside)
         loginButton.setTitleColor(.white, for: .normal)
         loginButton.setTitle("Login", for: .normal)
         loginButton.layer.cornerRadius = 10
@@ -69,15 +78,15 @@ extension LoginViewController {
         loginButton.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.width.equalTo(160)
-            make.top.equalTo(passwordField).inset(80)
+            make.top.equalTo(passwordField).inset(60)
             make.height.equalTo(40)
         }
         
-        
-        
-        
-        let signUpButton = UIButton()
-        signUpButton.backgroundColor = UIColor(red: 252/255, green: 149/255, blue: 107/255, alpha: 1)
+        signUpButton.backgroundColor = UIColor(red: 252/255,
+                                               green: 149/255,
+                                               blue: 107/255,
+                                               alpha: 1)
+        signUpButton.addTarget(self, action: #selector(signUpButtonPressed), for: .touchUpInside)
         signUpButton.setTitleColor(.white, for: .normal)
         signUpButton.setTitle("Sign Up", for: .normal)
         signUpButton.layer.cornerRadius = 10
@@ -88,10 +97,9 @@ extension LoginViewController {
             make.top.equalTo(loginButton).inset(50)
             make.height.equalTo(40)
         }
-        
-        let enterAsGuestButton = UIButton()
+    
         enterAsGuestButton.backgroundColor = passwordField.backgroundColor
-//        enterAsGuestButton.addTarget(self, action: #selector(enterAsGuestButtonPressed), for: .touchUpInside)
+        enterAsGuestButton.addTarget(self, action: #selector(enterAsGuestButtonPressed), for: .touchUpInside)
         enterAsGuestButton.setTitleColor(.lightGray, for: .normal)
         enterAsGuestButton.setTitle("Enter as a guest ", for: .normal)
         enterAsGuestButton.layer.cornerRadius = 10
@@ -103,6 +111,9 @@ extension LoginViewController {
             make.height.equalTo(40)
         }
     }
+    
+    
+    
     
     
     
--- Milky/Milky/Controllers/LoginViewController.swift
+++ Milky/Milky/Controllers/LoginViewController.swift
@@ -6,24 +6,140 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
+    
+    
+    let milkyLabel = UILabel()
+    let emailField = UITextField()
+    let passwordField = UITextField()
+    let loginButton = UIButton()
+    let signUpButton = UIButton()
+    let enterAsGuestButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupLoginViewUI()
         
+        if FirebaseAuth.Auth.auth().currentUser != nil {
+            milkyLabel.isHidden = true
+            emailField.isHidden = true
+            passwordField.isHidden = true
+            loginButton.isHidden = true
+            signUpButton.isHidden = true
+            enterAsGuestButton.isHidden = true
+            
+        }
+        
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        emailField.becomeFirstResponder()
+
+    }
+    
     
     @objc func loginButtonPressed(sender: UIButton!) {
         print("loginButtonPressed")
     }
     
+//    @objc func signUpButtonPressed(sender: UIButton!) {
+//        print("signUpButtonPressed")
+//    }
+    
     @objc func enterAsGuestButtonPressed(sender: UIButton!) {
         print("enterAsGuestButtonPressed")
     }
-
+    
+    
+    @objc func signUpButtonPressed(sender: UIButton!) {
+        print("Sign Up button tapped ")
+        guard let email = emailField.text, !email.isEmpty,
+                let password = passwordField.text, !password.isEmpty else {
+                    print("Missing field data")
+                    return
+                }
+        
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: { [weak self] result, error in
+            guard let strongSelf = self else {
+                return
+            }
+            
+            
+            guard error == nil else {
+                // show account creation
+                strongSelf.showCreateAccount(email: email, password: password)
+                return
+            }
+            
+            print("You have sign in")
+            strongSelf.milkyLabel.isHidden = true
+            strongSelf.emailField.isHidden = true
+            strongSelf.passwordField.isHidden = true
+            strongSelf.loginButton.isHidden = true
+            strongSelf.signUpButton.isHidden = true
+            strongSelf.enterAsGuestButton.isHidden = true
+            
+            strongSelf.emailField.resignFirstResponder()
+            strongSelf.passwordField.resignFirstResponder()
+        })
+        
+    }
+    
+    func showCreateAccount(email: String, password: String) {
+        let alert = UIAlertController(title: "Create Account",
+                                      message: "Would you like to create an account",
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Continue",
+                                      style: .default,
+                                      handler: {_ in
+            FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password, completion: { [weak self] result, error in
+                
+                guard let strongSelf = self else {
+                    return
+                }
+                
+                guard error == nil else {
+                    // show account creation
+                    print("Account creation failed")
+                    return
+                }
+                
+                print("You have sign in")
+                strongSelf.milkyLabel.isHidden = true
+                strongSelf.emailField.isHidden = true
+                strongSelf.passwordField.isHidden = true
+                strongSelf.loginButton.isHidden = true
+                strongSelf.signUpButton.isHidden = true
+                strongSelf.enterAsGuestButton.isHidden = true
+                
+                strongSelf.emailField.resignFirstResponder()
+                strongSelf.passwordField.resignFirstResponder()
+            })
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel",
+                                      style: .cancel,
+                                      handler: {_ in
+            
+        }))
+        
+        present(alert, animated: true)
+    }
+    
+    // get auth instance
+    // attempt sign in
+    // if failure, present alert to create account (OR send an arror)
+    // if user continues, create account
+    
+    // check sign in on APP launch
+    // allow user to sign upwith button
+    
+    
+   
 
 }
 
